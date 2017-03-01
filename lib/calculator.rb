3603d246701aadd4f76002d5784ea61bee02770e
Justin Teo <justin.tsw@gmail.com>
2017-03-01 15:33:26 +0800

    Accessors test done

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,7 @@
 class Calculator
   # your class goes here
+  def initialize(num)
+    @num = num
+  end
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -10,6 +10,25 @@ describe Calculator do
       it 'is an instance of the Calculator class' do
         expect(@calculator).to be_instance_of(Calculator)
       end
+  end
+
+# Results: my_calculator.result would return the current result - it should be readonly.
+    describe '.result' do
+      it 'should return current result' do
+        expect(@calculator.result).to eq(1)
+      end
     end
-  # Tests go here
+
+    describe 'Accessors' do
+      it 'should get result' do
+        expect(@calculator.result).to eq(1)
+      end
+
+      it 'should not set result' do
+        @calculator.result = 5 if respond_to? :result=   #Trying to set result to 5 here, and if there is a response to :result= method, we expect @calculator.result to still be 1,i.e. unchanged.
+        expect(@calculator.result).to eq(1)
+      end
+
+    end
+
 end
