2c1a315a34656fdf44a7e62d6a8cc44fcba95c3b
tgt87 <golduck21187@yahoo.com.sg>
2017-03-01 15:14:18 +0800

    initialise

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,7 +4,12 @@ require_relative '../lib/calculator'
 describe Calculator do
   before(:context) do
     # initialize calculator
+    @calculator = Calculator.new(1)
   end
-
+  describe 'Initialization' do
+      it 'is an instance of the Calculator class' do
+        expect(@calculator).to be_instance_of(Calculator)
+      end
+    end
   # Tests go here
 end
